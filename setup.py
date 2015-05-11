8472b230555cb0e2593468d5814eeb02f5b3f4da
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2015-05-11 17:46:37 -0400

    adding file

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-datagovtheme',
+	version=version,
+	description="Datagov Theme",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='REI',
+	author_email='',
+	url='',
+	license='',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.datagovtheme'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.datagovtheme:PluginClass
+        datagovtheme=ckanext.datagovtheme.plugin:DatagovTheme
+	""",
+)
