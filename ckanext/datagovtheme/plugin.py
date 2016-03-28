6f95e8852407b2b0adb29c93342a9f778a4a865e
fxia <fxia@reisystems.com>
2016-03-28 17:55:06 -0400

    make facet name plural

--- ckanext/datagovtheme/plugin.py
+++ ckanext/datagovtheme/plugin.py
@@ -33,8 +33,8 @@ class DatagovTheme(p.SingletonPlugin):
                             ('res_format', 'Formats'),
                             ('organization_type', 'Organization Types'),
                             ('organization', 'Organizations'),
-                            ('publisher', 'Publisher'),
-                            ('bureauCode', 'Bureau'),
+                            ('publisher', 'Publishers'),
+                            ('bureauCode', 'Bureaus'),
                            ## ('extras_progress', 'Progress'),
                            ])
 
@@ -50,8 +50,8 @@ class DatagovTheme(p.SingletonPlugin):
                                 ('harvest_source_title', 'Harvest Source'),
                                 ('capacity', 'Visibility'),
                                 ('dataset_type', 'Resource Type'),
-                                ('publisher', 'Publisher'),
-                                ('bureauCode', 'Bureau'),
+                                ('publisher', 'Publishers'),
+                                ('bureauCode', 'Bureaus'),
                                ])
         else:
             return facets_dict
