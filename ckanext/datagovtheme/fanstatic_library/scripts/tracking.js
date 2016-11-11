11e08d3ea571429398515a8281c2229f538fb40c
fxia <fxia@reisystems.com>
2016-11-11 12:24:29 -0500

    fix some tracking issues

--- ckanext/datagovtheme/fanstatic_library/scripts/tracking.js
+++ ckanext/datagovtheme/fanstatic_library/scripts/tracking.js
@@ -2,17 +2,12 @@ $(function (){
   // Tracking
   var url = location.pathname;
   // remove any site root from url
-//  console.log($('body').data('site-root'));
   url = url.substring($('body').data('locale-root'), url.length);
   // trim any trailing /
   url = url.replace(/\/*$/, '');
-  $.ajax({url : $('body').data('site-root') + '_tracking',
-          type : 'POST',
-          data : {url:url, type:'page'},
-          timeout : 300 });
   $('a.resource-url-analytics').click(function (e){
     var url = $(e.target).closest('a').attr('href');
-    $.ajax({url : $('body').data('site-root') + '_tracking',
+    $.ajax({url : '/_tracking',
             data : {url:url, type:'resource'},
             type : 'POST',
             complete : function () {location.href = url;},
@@ -22,12 +17,12 @@ $(function (){
 
   $('div.btn-group a.btn.btn-primary').each(function(){
         if($(this).find('i').attr('class') == 'icon-external-link' || $(this).find('i').attr('class') == 'icon-download-alt') {
-            $(this).click(function(){
+            $(this).click(function(e){
                 var url = $(this).closest('a').attr('href');
-                $.ajax({url : $('body').data('site-root') + '_tracking',
+                $.ajax({url : '/_tracking',
                     data : {url:url, type:'resource'},
                     type : 'POST',
-                    //complete : function () {location.href = url;},
+                    complete : function () {location.href = url;},
                     timeout : 30});
                 e.preventDefault();
             });
