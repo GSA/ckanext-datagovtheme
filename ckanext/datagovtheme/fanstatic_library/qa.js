819085365c5edf0e36743fa24a7c73ecccdce448
root <john.yu@reisystems.com>
2017-06-27 13:27:06 -0400

    tables and icons added

--- ckanext/datagovtheme/fanstatic_library/qa.js
+++ ckanext/datagovtheme/fanstatic_library/qa.js
@@ -23,4 +23,14 @@ this.jQuery(function (jQuery) {
       link.text(link.data('showText'));
     }
   });
+  $('[data-toggle="qa_tooltip"]').tooltip({
+	title: "<ul class='tooltip1' style='font-size:.8em; list-style:none; margin:0px; z-index:99;'><li><strong>Scoring Methodology</strong></li><br><li>&#9733; - Available under an open license</li><br><li>&#9733;&#9733; - Available as structured data (eg. Excel instead of a scanned table)</li><br><li>&#9733;&#9733;&#9733; - Uses non-proprietary formats (e.g., CSV instead of Excel)</li><br><li>&#9733;&#9733;&#9733;&#9733; - 'Uses URIs to identify things, so that people can link to it</li><br><li>&#9733;&#9733;&#9733;&#9733;&#9733; - Linked to other data to provide context</li></ul>",
+	opacity: 1,
+	delay: { show: 400, hide: 200 }
+  });
+  $('[data-toggle="qa_tooltip2"]').tooltip({
+        title: "<ul class='tooltip2' style='font-size:.8em; list-style:none; margin:0px; z-index:99;'><li><strong>Scoring Methodology</strong></li><br><li>&#9733; - Available under an open license</li><br><li>&#9733;&#9733; - Available as structured data (eg. Excel instead of a scanned table)</li><br><li>&#9733;&#9733;&#9733; - Uses non-proprietary formats (e.g., CSV instead of Excel)</li><br><li>&#9733;&#9733;&#9733;&#9733; - 'Uses URIs to identify things, so that people can link to it</li><br><li>&#9733;&#9733;&#9733;&#9733;&#9733; - Linked to other data to provide context</li></ul>",
+        opacity: 1,
+        delay: { show: 400, hide: 200 }
+  });
 });
