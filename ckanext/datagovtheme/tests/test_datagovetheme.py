404e0022e1e21a9d0c6df68da4f940d316654485
Julie Kramer <thejuliekramer@gmail.com>
2019-11-12 11:53:14 -0500

    add datagovtheme test

---
+++ ckanext/datagovtheme/tests/test_datagovetheme.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+'''Tests for the ckanext.datagovtheme extension.
+
+'''
+from bs4 import BeautifulSoup
+from nose.tools import assert_true
+
+import ckan.tests.helpers as helpers
+
+
+class TestDatagovthemeServed(helpers.FunctionalTestBase):
+    '''Tests for the ckanext.datagovtheme.plugin module.'''
+
+    def test_datagovtheme_css(self):
+        app = helpers._get_test_app()
+
+        index_response = app.get('/')
+
+        assert_true('datagovtheme.js' in index_response)
+        assert_true('datagovtheme.css' in index_response)
