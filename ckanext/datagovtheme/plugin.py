00ef64786cf5751eff18bbd468a913862698617c
Andres Vazquez <andres@data99.com.ar>
2020-06-02 10:30:18 -0300

    use config to avoid UI errors with upstreramed extensions (#43)
    * use config to avoid UI errors with upstream extensions
    
    * Add new config values to allow select which extensions functions shows in UI

--- ckanext/datagovtheme/plugin.py
+++ ckanext/datagovtheme/plugin.py
@@ -106,4 +106,5 @@ class DatagovTheme(p.SingletonPlugin):
             'schema11_frequency_mod':datagovtheme_helpers.schema11_frequency_mod,
             'convert_top_category_to_list':datagovtheme_helpers.convert_top_category_to_list,
             'is_bootstrap2':datagovtheme_helpers.is_bootstrap2,
+            'use_extension':datagovtheme_helpers.use_extension,
         }
