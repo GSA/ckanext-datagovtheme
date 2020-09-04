eb1e5fb4b6db7d10d581da7f210bfc610cb8b376
Andres Vazquez <andres@data99.com.ar>
2020-09-04 12:44:27 -0300

    Py 2.7.10

--- bin/travis-build-CKAN-2.3.bash
+++ bin/travis-build-CKAN-2.3.bash
@@ -2,16 +2,60 @@
 set -e
 
 echo "This is travis-build.bash..."
-
 echo "-----------------------------------------------------------------"
-echo "Installing the packages that CKAN requires..."
+
+echo "Moving to Python 2.7.10"
 sudo apt-get update -qq
-sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib swig libgeos-dev
+sudo apt-get install -y gcc-multilib g++-multilib libffi-dev libffi6 libffi6-dbg \
+    python-crypto python-mox3 python-pil python-ply libssl-dev zlib1g-dev libbz2-dev \
+    libexpat1-dev libbluetooth-dev libgdbm-dev dpkg-dev quilt autotools-dev \
+    libreadline-dev libtinfo-dev libncursesw5-dev tk-dev blt-dev libssl-dev zlib1g-dev \
+    libbz2-dev libexpat1-dev libbluetooth-dev libsqlite3-dev libgpm2 mime-support \
+    netbase net-tools bzip2 solr-jetty libcommons-fileupload-java libpq-dev \
+    postgresql postgresql-contrib swig libgeos-dev python-pastescript
+
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar xvf Python-2.7.10.tgz
+cd Python-2.7.10/
+
+./configure --prefix /usr/local/lib/python2.7.10 --enable-ipv6 --enable-unicode=ucs4
+make
+sudo make install
+
+sudo rm /usr/bin/python
+sudo ln -s /usr/local/lib/python2.7.10/bin/python /usr/bin/python
+export PATH="/usr/local/lib/python2.7.10/bin:$PATH"
+
+cd ..
+# -------------------
+echo 'Getting Python version:'
+python -V
+
+echo 'Getting Python path:'
+which python
+
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
+
+echo 'Preparing PIP ...'
+sudo -H python -m pip install --upgrade pip
+sudo -H python -m pip install setuptools -U
+sudo -H python -m pip install wheel
+
+echo "nosetests"
+sudo -H python -m pip install nose
+which nosetests
 
 echo "-----------------------------------------------------------------"
 echo "Installing CKAN and its dependencies..."
+# install all from catalog-app repo
+wget https://raw.githubusercontent.com/GSA/catalog-app/master/requirements-freeze.txt
+sed -i '/saml2/d' requirements-freeze.txt
+sudo -H python -m pip install -r requirements-freeze.txt
+sudo -H python -m pip install -U repoze.who==2.0 Paste==1.7.5.1
 
-cd .. # CircleCI starts inside ckanext-datajson folder
+echo "Cloning CKAN ..."
+cd .. # CircleCI starts inside ckanext-datagovtheme folder
 pwd
 ls -la
 
@@ -20,18 +64,10 @@ cd ckan
 git checkout datagov
 
 echo "-----------------------------------------------------------------"
-echo "Installing Python dependencies..."
+echo "Installing CKAN ..."
 
-pip install --upgrade pip
-pip install setuptools -U
-
-python setup.py develop
 # TODO remove after upgrading to CKAN 2.8
 cp ./ckan/public/base/css/main.css ./ckan/public/base/css/main.debug.css
-pip install wheel
-
-# install all from catalog-app repo
-pip install -r https://raw.githubusercontent.com/GSA/catalog-app/master/requirements-freeze.txt
 
 cd ..
 echo "-----------------------------------------------------------------"
@@ -48,29 +84,32 @@ echo "-----------------------------------------------------------------"
 echo "Creating the PostgreSQL user and database..."
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
-sudo -u postgres psql -c 'CREATE DATABASE datastore_test WITH OWNER ckan_default;'
 
 echo "-----------------------------------------------------------------"
 echo "Initialising the database..."
 cd ckan
-paster db init -c test-core.ini
+sudo paster --plugin=ckan db init -c test-core.ini
 echo "-----------------------------------------------------------------"
 echo "Initialising the report ..."
-paster --plugin=ckanext-report report initdb -c ../ckan/test-core.ini
+sudo paster --plugin=ckanext-report report initdb -c test-core.ini
 echo "-----------------------------------------------------------------"
 echo "Initialising the archiver ..."
-paster --plugin=ckanext-archiver archiver init -c ../ckan/test-core.ini
+sudo paster --plugin=ckanext-archiver archiver init -c test-core.ini
 echo "-----------------------------------------------------------------"
 echo "Initialising the harvester ..."
-paster --plugin=ckanext-harvest harvester initdb -c ../ckan/test-core.ini
+sudo paster --plugin=ckanext-harvest harvester initdb -c test-core.ini
 echo "-----------------------------------------------------------------"
 echo "Initialising the qa ..."
-paster --plugin=ckanext-qa qa init -c ../ckan/test-core.ini
+sudo paster --plugin=ckanext-qa qa init -c test-core.ini
 
 cd ..
 echo "-----------------------------------------------------------------"
 echo "Installing ckanext-datagovtheme and its requirements..."
 cd ckanext-datagovtheme
-python setup.py develop
+sudo python setup.py develop
 
 echo "travis-build.bash is done."
+
+echo "TESTING ckanext-datagovtheme with CKAN 2.3 and Bootstrap 2"
+sudo -H python -m pip install -r dev-requirements.txt
+nosetests --ckan --with-pylons=test-catalog-2.3.ini ckanext/datagovtheme/tests --nologcapture
--- bin/travis-run-tests-CKAN-2.3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-echo "TESTING ckanext-datagovtheme with CKAN 2.3 and Bootstrap 2"
-
-nosetests --ckan --with-pylons=test-catalog-2.3.ini ckanext/datagovtheme/tests --nologcapture
-
