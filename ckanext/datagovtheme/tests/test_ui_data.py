5d95d2a864104c8ac27aadf7d7953cc2894b7f34
Aaron D Borden <aaron.borden@gsa.gov>
2021-05-04 16:42:48 -0700

    fixup

--- ckanext/datagovtheme/tests/test_ui_data.py
+++ ckanext/datagovtheme/tests/test_ui_data.py
@@ -59,7 +59,7 @@ class TestUIData(FunctionalTestBase):
         html = BeautifulSoup(index_response.unicode_body, 'html.parser')
 
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 ' \
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 '
                           'Safari/537.3'
         }
         # Test link to api docs in CKAN 2.8
