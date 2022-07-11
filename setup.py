41f76c905ff93fb84b26d34958b00a986b8f015a
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-07-11 14:38:39 -0400

    new: add config to publish to pypi
    Update setup.py, contact information, github action config, requirements

--- setup.py
+++ setup.py
@@ -1,23 +1,34 @@
 from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
 
-version = '0.1'
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='ckanext-datagovtheme',
-    version=version,
-    description="Datagov Theme",
-    long_description="",
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    version='0.1.0',
+    description="CKAN Extension to manage data.gov theme",
+    long_description=long_description,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
-    author='REI',
-    author_email='',
-    url='',
-    license='',
+    author='Data.gov',
+    author_email='datagovhelp@gsa.gov',
+    url='https://github.com/GSA/ckanext-datagovtheme/',
+    license='Public Domain',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[],
+    install_requires=[
+        'future',
+    ],
+    setup_requires=['wheel'],
     entry_points="""
         [ckan.plugins]
         datagovtheme=ckanext.datagovtheme.plugin:DatagovTheme
