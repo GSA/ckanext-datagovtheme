f0699788f12c01d63f55a0ff22591eb8812d424a
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-06 11:28:27 -0400

    fix: relative imports

--- ckanext/datagovtheme/plugin.py
+++ ckanext/datagovtheme/plugin.py
@@ -3,7 +3,7 @@ from sqlalchemy.util import OrderedDict
 
 p.toolkit.requires_ckan_version("2.9")
 
-import blueprint
+from . import blueprint
 
 
 class DatagovTheme(p.SingletonPlugin):
@@ -18,15 +18,9 @@ class DatagovTheme(p.SingletonPlugin):
 
     # IConfigurer
     def update_config(self, config):
-        # TODO remove template/templates_2_8 and move templates/templates_new
-        # to templates once we're off of CKAN 2.8.
-        #
-        # Using a separate dir for templates avoids having to maintain
-        # backwards compatibility using a sprinkling of conditionals. We don't
-        # anticipate adding new features to the existing 2.8 templates.
-        p.toolkit.add_template_directory(config, '../templates/')
-        p.toolkit.add_public_directory(config, '../public')
-        p.toolkit.add_resource('../fanstatic_library', 'datagovtheme')
+        p.toolkit.add_template_directory(config, 'templates')
+        p.toolkit.add_public_directory(config, 'public')
+        p.toolkit.add_resource('fanstatic_library', 'datagovtheme')
 
     # IFacets
     def dataset_facets(self, facets_dict, package_type):
