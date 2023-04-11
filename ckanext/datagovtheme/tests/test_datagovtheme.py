e529a36ede0b2bca2ef24a8c37d9a03b91cb58f1
Fuhu Xia <fxia@reisystems.com>
2023-04-11 10:16:03 -0400

    pass the test

--- ckanext/datagovtheme/tests/test_datagovtheme.py
+++ ckanext/datagovtheme/tests/test_datagovtheme.py
@@ -65,31 +65,18 @@ class TestDatagovthemeServed(object):
     def test_datagovtheme_html_loads(self, app):
         index_response = app.get('/dataset')
 
-        assert "Search Data.Gov" in index_response.body
+        assert "An official website of the United States government" in index_response.body
         assert "Search datasets..." in index_response.body
         assert "datasets found" in index_response.body
 
     def test_datagovtheme_navigation(self, app):
         index_response = app.get('/dataset')
 
-        assert '<li class="active"><a href="/dataset">Data</a></li>' in index_response.body
-        assert '<a class="dropdown-toggle" data-toggle="dropdown">Topics<b\n            class="caret"></b></a>' \
-               in index_response.body
-        assert '<li><a href="//resources.data.gov">Resources</a></li>' in index_response.body
-        assert '<li><a href="//strategy.data.gov">Strategy</a></li>' in index_response.body
-        assert '<li><a href="//www.data.gov/developers/">Developers</a></li>' in index_response.body
-        assert '<li><a href="//www.data.gov/contact">Contact</a></li>' in index_response.body
-
-    def test_datagovtheme_topics(self, app):
-        index_response = app.get('/dataset')
-
-        assert '<li class="menu-agriculture">' in index_response.body
-        assert '<li class="menu-climate">' in index_response.body
-        assert '<li class="menu-energy">' in index_response.body
-        assert '<li class="menu-local-government">' in index_response.body
-        assert '<li class="menu-maritime">' in index_response.body
-        assert '<li class="menu-ocean">' in index_response.body
-        assert '<li class="menu-older-adults-health">' in index_response.body
+        assert '<span class="text-uppercase">Data</span></a>' in index_response.body
+        assert '<span class="text-uppercase">Reports</span></a>' in index_response.body
+        assert '<span class="text-uppercase">Open Government</span></a>' in index_response.body
+        assert '<span class="text-uppercase">Contact</span></a>' in index_response.body
+        assert '<span class="text-uppercase">User Guide</span></a>' in index_response.body
 
     def test_datagovtheme_organizations(self, app):
         self.create_datasets(self.get_base_dataset())
