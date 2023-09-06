e0b1bdac97855593585b47f5b056cc07657c2800
Tyler Burton <tyler.burton@gsa.gov>
2023-09-06 10:14:23 -0500

    bump pypi version

--- setup.py
+++ setup.py
@@ -5,29 +5,28 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='ckanext-datagovtheme',
-    version='0.2.3',
+    name="ckanext-datagovtheme",
+    version="0.2.4",
     description="CKAN Extension to manage data.gov theme",
     long_description=long_description,
     classifiers=[
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
     ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='Data.gov',
-    author_email='datagovhelp@gsa.gov',
-    url='https://github.com/GSA/ckanext-datagovtheme/',
-    license='Public Domain',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext'],
+    keywords="",
+    author="Data.gov",
+    author_email="datagovhelp@gsa.gov",
+    url="https://github.com/GSA/ckanext-datagovtheme/",
+    license="Public Domain",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    namespace_packages=["ckanext"],
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-    ],
-    setup_requires=['wheel'],
+    install_requires=[],
+    setup_requires=["wheel"],
     entry_points="""
         [ckan.plugins]
         datagovtheme=ckanext.datagovtheme.plugin:DatagovTheme
