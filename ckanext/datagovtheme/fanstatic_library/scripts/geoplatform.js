1f651da2d21574f6e3d1e42e3e183349ba45c6d7
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-11 09:54:34 -0700

    wrap function declaration in dom load.

--- ckanext/datagovtheme/fanstatic_library/scripts/geoplatform.js
+++ ckanext/datagovtheme/fanstatic_library/scripts/geoplatform.js
@@ -1,28 +1,28 @@
-async function getGeoplatformLink() {
-    let parentEl = document.getElementById("geoplatform-link-section");
-    if (parentEl == null) {
-        return;
-    }
-    let datasetName = parentEl.getAttribute("data-package-name");
-    try {
-        const response = await fetch(
-            `https://stg-api.geoplatform.gov/v3/public/lookups/data-gov/dataset?name=${datasetName}`
-        )
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                let el = document.getElementById("geoplatform-link");
-                el.href = data.geoplatform_url;
-                parentEl.classList.remove("hide");
-            });
-    } catch (error) {
-        console.error(error);
-    }
-}
-
 jQuery(function ($) {
+    async function getGeoplatformLink() {
+        let parentEl = document.getElementById("geoplatform-link-section");
+        if (parentEl == null) {
+            return;
+        }
+        let datasetName = parentEl.getAttribute("data-package-name");
+        try {
+            const response = await fetch(
+                `https://stg-api.geoplatform.gov/v3/public/lookups/data-gov/dataset?name=${datasetName}`
+            )
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                })
+                .then((data) => {
+                    let el = document.getElementById("geoplatform-link");
+                    el.href = data.geoplatform_url;
+                    parentEl.classList.remove("hide");
+                });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     getGeoplatformLink();
 });
