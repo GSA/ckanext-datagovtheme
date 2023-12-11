3af066f1fefb8e67946b7de0728d33e38885147a
Reid Hewitt <sam.hew.r@gmail.com>
2023-12-11 11:03:03 -0700

    handle/log timeout, 404, and 500s

--- ckanext/datagovtheme/fanstatic_library/scripts/geoplatform.js
+++ ckanext/datagovtheme/fanstatic_library/scripts/geoplatform.js
@@ -7,11 +7,20 @@ jQuery(function ($) {
         let datasetName = parentEl.getAttribute("data-package-name");
         try {
             const response = await fetch(
-                `https://stg-api.geoplatform.gov/v3/public/lookups/data-gov/dataset?name=${datasetName}`
+                `https://stg-api.geoplatform.gov/v3/public/lookups/data-gov/dataset?name=${datasetName}`,
+                { signal: AbortSignal.timeout(20000) }
             )
                 .then((response) => {
                     if (response.ok) {
                         return response.json();
+                    }
+                    if (response.status == 404) {
+                        console.log("dataset doesn't exist on geoplatform");
+                    }
+                    if (response.status >= 500) {
+                        console.warn(
+                            "geoplatform couldn't fulfill the request"
+                        );
                     }
                 })
                 .then((data) => {
@@ -20,7 +29,11 @@ jQuery(function ($) {
                     parentEl.classList.remove("hide");
                 });
         } catch (error) {
-            console.error(error);
+            if (error.name === "TimeoutError") {
+                console.error("request timeout. geoplatform not reachable");
+            } else {
+                console.error(error);
+            }
         }
     }
 
