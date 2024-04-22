6a6a0a08f608045ac0b9ecf9a3888d7563a69eca
Daniel Lennox <dlennox2424@gmail.com>
2024-04-22 14:51:50 -0600

    Adding org and publisher distinction in gtm datalayer

--- ckanext/datagovtheme/fanstatic_library/scripts/gtmCustomizations.js
+++ ckanext/datagovtheme/fanstatic_library/scripts/gtmCustomizations.js
@@ -3,19 +3,38 @@ if (window.location.pathname.match(/^\/dataset\/.*$/g)) {
   const getPublisher = () => {
     const nodeList = document.querySelectorAll(`[property="dct:publisher"]`);
     if (nodeList.length === 1) {
-      return nodeList[0]?.innerHTML.replaceAll(/<[^>]*>/gi, '');
+      return nodeList[0]?.innerHTML.replaceAll(/<[^>]*>/gi, '').trim();
     }
 
-    // fall through that we may not need
-    return 'err - multiple or no publishers';
+    return 'err - multiple or no publisher';
+  };
+
+  const getOrgKeys = () => {
+    const idNodeList = document.querySelectorAll('div[class="org_type"] a');
+    const nameNodeList = document.querySelectorAll('div[class="org_type"] h1[class="heading"]');
+    const keys = {
+      organizationId: 'err - multiple or no organization',
+      organizationName: 'err - multiple or no organization',
+    };
+
+    if (idNodeList.length === 1) {
+      keys.organizationId = idNodeList[0]?.getAttribute('href').split('/organization/').pop();
+    }
+
+    if (nameNodeList.length === 1) {
+      keys.organizationName = nameNodeList[0]?.innerHTML.trim();
+    }
+
+    return keys;
   };
 
   window.dataLayer = window.dataLayer || [];
-  const publisherObjIndex = window.dataLayer.findIndex((o) => o.id === 'datasetPublisher');
+  const publisherObjIndex = window.dataLayer.findIndex((o) => o.id === 'customDatasetKeys');
   const publisherObj = {
-    id: 'datasetPublisher',
+    id: 'customDatasetKeys',
     event: 'new_view',
-    agency: getPublisher(),
+    publisher: getPublisher(),
+    ...getOrgKeys(),
     dataSetName: document.querySelectorAll('h1[itemprop="name"]')[0]?.innerText || null,
     uri: window.location.pathname,
   };
