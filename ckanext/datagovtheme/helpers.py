c738f0a66e9ddad77d0e9749d1c4090e7fa3dc07
Fuhu Xia <fxia@reisystems.com>
2024-07-08 11:25:06 -0400

    optional ckanext.datagovtheme.harvest_next for catalog-next

--- ckanext/datagovtheme/helpers.py
+++ ckanext/datagovtheme/helpers.py
@@ -261,7 +261,14 @@ def render_datetime_datagov(date_str):
     return value
 
 
-def get_harvest_object_formats(harvest_object_id):
+def get_harvest_object_formats(harvest_object_id, dataset_is_datajson=False):
+    # simplified return for harvest_next
+    harvest_next = asbool(config.get('ckanext.datagovtheme.harvest_next', 'false'))
+    if harvest_next:
+        return {
+            'object_format': 'data.json' if dataset_is_datajson else 'ISO-19139'
+        }
+
     try:
         obj = p.toolkit.get_action('harvest_object_show')({}, {'id': harvest_object_id})
     except p.toolkit.ObjectNotFound:
@@ -321,7 +328,11 @@ def get_harvest_source_link(package_dict):
 
     if harvest_source_id and harvest_source_title:
         msg = p.toolkit._('Harvested from')
-        url = h.url_for('harvest_read', id=harvest_source_id)
+        harvest_next = asbool(config.get('ckanext.datagovtheme.harvest_next', 'false'))
+        if harvest_next:
+            url = h.url_for(f'/harvest/{harvest_source_id}')
+        else:
+            url = h.url_for('harvest_read', id=harvest_source_id)
         link = '{msg} <a href="{url}">{title}</a>'.format(url=url, msg=msg, title=harvest_source_title)
         return p.toolkit.literal(link)
 
@@ -679,8 +690,10 @@ def get_pkg_dict_extra(pkg_dict, key, default=None):
                 if k == key:
                     return value
 
+    harvest_next = asbool(config.get('ckanext.datagovtheme.harvest_next', 'false'))
     # Also include harvest information if exists
-    if key in ['harvest_object_id', 'harvest_source_id', 'harvest_source_title']:
+    if key in ['harvest_object_id', 'harvest_source_id', 'harvest_source_title'] \
+            and not harvest_next:
 
         harvest_object = model.Session.query(HarvestObject) \
                 .filter(HarvestObject.package_id == pkg_dict['id']) \
