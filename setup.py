3af89afea32dcb5616298cb731d91a89642ebb34
Fuhu Xia <fxia@reisystems.com>
2025-03-24 11:07:35 -0400

    bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="ckanext-datagovtheme",
-    version="0.2.43",
+    version="0.3.0",
     description="CKAN Extension to manage data.gov theme",
     long_description=long_description,
     classifiers=[
