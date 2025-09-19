5e9cc4c042c52b92e2b3e3f3bee3dfaff58c5337
Fuhu Xia <fxia@reisystems.com>
2025-09-19 12:37:56 -0400

    fix test

--- cypress/e2e/facets_bureaus_name.cy.js
+++ cypress/e2e/facets_bureaus_name.cy.js
@@ -1,8 +1,8 @@
 describe('Facets Bureaus', () => {
     it('Check Bureaus name is string', () => {
         cy.visit('/dataset');
-        cy.get('.filters h2').its('length').should('be.equal', 9);
-        cy.get('.filters h2').eq(8).contains('Bureaus');
+        cy.get('.filters h2').its('length').should('be.equal', 7);
+        cy.get('.filters h2').eq(6).contains('Bureaus');
 
         cy.get('nav[aria-label="Bureaus"] span[class="item-label"]')
             .each(($el) => {
